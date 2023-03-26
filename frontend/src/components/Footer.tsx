cb1b60f5fa5c9922b5ea76e3ca01c1e1a343b9d8
pom421 <mauguet.po@gmail.com>
2023-03-27 00:09:36 +0200

    feat: dark mode for footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,11 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react"
 
 const Footer = () => {
+  const bg = useColorModeValue("blue.400", "blue.700")
+  const color = useColorModeValue("white", "gray.100")
+
   return (
-    <Flex justifyContent="center" alignItems="center" width="100%" height="10vh" p="2rem" backgroundColor={"lightblue"}>
+    <Flex justifyContent="center" alignItems="center" width="100%" height="10vh" p="2rem" bg={bg} color={color}>
       <Text>&copy; Pom421 {new Date().getFullYear()}</Text>
     </Flex>
   )
