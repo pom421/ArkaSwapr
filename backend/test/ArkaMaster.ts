7543faa8ec68f6903a1d5a94bfc6ffe462befd8b
pom421 <mauguet.po@gmail.com>
2023-04-07 11:53:14 +0200

    feat: add tests for ArkaMaster

--- backend/test/ArkaMaster.ts
+++ backend/test/ArkaMaster.ts
@@ -1,124 +1,85 @@
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
+const CHAIN_LINK_ADDRESS = "0x694AA1769357215DE4FAC081bf1f309aDC325306"; // Sepolia
+
 describe("ArkaMaster", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
+  async function deployArkaContracts() {
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner, account1] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    // Oracle
+    const Oracle = await ethers.getContractFactory("ChainlinkEthUsd");
+    const oracle = await Oracle.deploy(CHAIN_LINK_ADDRESS);
+    await oracle.deployed();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    // ArkaERC20
+    const ArkaERC20 = await ethers.getContractFactory("ArkaERC20");
+    const arkaERC20 = await ArkaERC20.deploy();
+    await arkaERC20.deployed();
+
+    // ArkaMaster
+    const ArkaMaster = await ethers.getContractFactory("ArkaMaster");
+    const arkaMaster = await ArkaMaster.deploy(
+      arkaERC20.address,
+      oracle.address
+    );
+    await arkaMaster.deployed();
+
+    return { owner, account1, oracle, arkaERC20, arkaMaster };
   }
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+  describe("ArkaERC20 tests", function () {
+    it("sets correctly the address of ArkaMaster, test storage", async function () {
+      const { arkaERC20, arkaMaster } = await loadFixture(deployArkaContracts);
+      await arkaERC20.setArkaMaster(arkaMaster.address);
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+      expect(await arkaERC20.arkaMaster()).to.equal(arkaMaster.address);
     });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
+    it("sets incorrectly the arkaMaster's address but he is not owner, test revert", async function () {
+      const { account1, arkaERC20, arkaMaster } = await loadFixture(
+        deployArkaContracts
       );
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
+      await expect(
+        arkaERC20.connect(account1).setArkaMaster(arkaMaster.address)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
     });
 
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
+    it("has the right initial supply, test storage", async function () {
+      const { arkaERC20 } = await loadFixture(deployArkaContracts);
+
+      expect(await arkaERC20.totalSupply()).to.equal(
+        BigNumber.from("1000000000000000000")
       );
     });
+    // Problem because arkaMaster contract has no eth balance to mint. Need to test it in ArkaMaster.interact.
+    // it("Should mint ArkaERC20 only for ArkaMaster address", async function () {
+    //   const { account1, arkaERC20, arkaMaster } = await loadFixture(
+    //     deployArkaContracts
+    //   );
+    //   await arkaERC20.setArkaMaster(arkaMaster.address);
+
+    //    expect(await arkaERC20.balanceOf(account1.address)).to.equal(0);
+
+    //   const arkaMasterSigner = await ethers.getImpersonatedSigner(arkaMaster.address);
+
+    //   await arkaERC20.connect(arkaMasterSigner).mintArka(account1.address, 5);
+
+    //    expect(await arkaERC20.balanceOf(account1.address)).to.equal(0);
+    // });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("ArkaMaster tests", function () {
+    it("sets correctly arkaToken and oracle addresses, test storage", async function () {
+      const { arkaERC20, arkaMaster, oracle } = await loadFixture(
+        deployArkaContracts
+      );
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+      expect(await arkaMaster.arkaToken()).to.equal(arkaERC20.address);
+      expect(await arkaMaster.priceFeedEthUsd()).to.equal(oracle.address);
     });
   });
 });
